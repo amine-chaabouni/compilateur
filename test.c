ef1e06dbecc14fcb088427dfd8fa3e0e37725e1a
amine-chaabouni <amine.chaabouni@polytechnique.edu>
2021-03-01 16:12:03 +0100

    modified exe file

--- test.c
+++ test.c
@@ -1,14 +1,7 @@
 
 // optimiser 0 / e en 0 quand e est pure n'est pas correct
 // si e peut être nulle
-int f(int x, int y, int z, int a, int b, int c, int d){
-  x = 2;
-  return 0;
-}
 
 int main() {
-  int x;
-  x = 2/3;
-  x = f(1,2,3,4,5,6,7);
-  return 0;
+  return 42;
 }
