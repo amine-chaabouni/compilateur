a58233ec72bf475bdab5cf80a229e4a1bab4109e
amine-chaabouni <amine.chaabouni@polytechnique.edu>
2021-03-11 12:19:42 +0100

    add constant case in branching and clean some comments

--- rtl.ml
+++ rtl.ml
@@ -5,7 +5,7 @@ open Rtltree
 let zero_i32 = Int32.of_int 0;;
 let one_i32 = Int32.of_int 1;;
 
-let wordsize = 8;;
+let word_size = 8;;
 
 exception Error of string
 
@@ -20,18 +20,16 @@ Hashtbl.add functions "sbrk" ("n"::[]);;
 let var_to_reg = Stack.create();;
 
 exception Register_Found of Register.t
-exception New_variable of Register.t
-
-
 let associate_register id =
-  let aux x = try
-    raise (Register_Found (Hashtbl.find x id))
+  let find_in_block block_hashtbl = try
+    raise (Register_Found (Hashtbl.find block_hashtbl id))
   with Not_found -> ()
   in try
-    Stack.iter aux var_to_reg;
+    Stack.iter find_in_block var_to_reg;
     raise_error ("Undeclared variable " ^ id);
   with Register_Found x -> x;;
 
+(* RTL graph*)
 let graph = ref Label.M.empty
 
 let generate i =
@@ -39,21 +37,36 @@ let generate i =
   graph := Label.M.add l i !graph;
   l
 
+(* See the variables inside a certain hashtable *)
 let print_vars ht =
   Hashtbl.iter (fun x y -> Printf.printf "%s\n" x) ht
 
 
 
-(*e est l'expression à traduire,
-  destr le registre de destination de la valeur de cette expression
-  destl l'étiquette où il faut transférer ensuite le contrôle
-  last_id is used to treat the instuction [int x = x] for exemple using different x*)
+(*e is the expression to be converted,
+destr is the register of destinition to the expression's value
+destl is the label where the control should be transfered after the conversion*)
+  
+(* An implementation can is proposed for a certain feature :
+let's say we have this function : 
+1 int f(){
+2   int x = 65;
+3   { int x = x + 1; putchar(x); }
+4   putchar(x);
+5   return 0;
+6 }
+In C, we have written an undefined behaviour. the result will be @A where @ can be anything.
+This is what has been implemented.
+However, we can change the compiler to have line 3 use the previously declared x before declaring
+a new x inside the new block. The result would be BA.
+In order to do that, we uncomment the last argument (last_id) of the function expr
+last_id is used to treat the instuction [int x = x], for exemple, using different x*)
 let rec expr e destr destl (*last_id*) = match e with
   | Ttree.Econst cst -> generate (Rtltree.Econst(cst, destr, destl))
   | Ttree.Eunop (unop, e) -> treat_unop e destr destl unop
   | Ttree.Ebinop (binop, e1, e2) -> begin
     match binop with
-    (*Treat && and || as a branch*)
+    (* Treat && and || as a branch *)
     | Ptree.Band | Ptree.Bor -> 
       let truel = Label.fresh() and falsel = Label.fresh() in
       let label = condition e truel falsel in
@@ -63,35 +76,34 @@ let rec expr e destr destl (*last_id*) = match e with
     | Ptree.Beq  -> treat_immediate e1 e2 destr destl Ops.Msete (*last_id*)
     | Ptree.Bneq  -> treat_immediate e1 e2 destr destl Ops.Msetne (*last_id*)
     | Ptree.Badd -> treat_immediate e1 e2 destr destl Ops.Madd (*last_id*)
-    (*For the rest, treat it normally*)
+    (* For the rest, treat it normally *)
     | _ -> treat_binop e1 e2 destr destl binop (*last_id*)
   end;
   | Ttree.Eassign_local (id, e) -> begin
-    (* Find the register associated to the variable.
-    If the register doesn't exist already, create a fresh one
-    Add the association to the table and add the register to the locals*)
     (*let actual_block =  if (id = last_id) then 
       try
         Stack.pop var_to_reg
       with Stack.Empty -> raise_error("Empty here access local")
     else Hashtbl.create 1 in*)
+
+    (* Find the register associated to the variable *)
     let register = associate_register id in
     
     let instruction_assign = Rtltree.Embinop(Ops.Mmov, destr, register, destl) in
     let label_assign = generate instruction_assign in
     let label_expression = expr e.expr_node destr label_assign (*""*) in
+
     (*if(id = last_id) then Stack.push actual_block var_to_reg;*)
     label_expression;
   end;
   | Ttree.Eaccess_local id -> begin
-    (* Find the register associated to the variable.
-    If the register doesn't exist already, create a fresh one
-    Add the association to the table and add the register to the locals*)
     (*let actual_block =  if (id = last_id) then 
       try
         Stack.pop var_to_reg
       with Stack.Empty -> raise_error("Empty here access local")
     else Hashtbl.create 1 in*)
+
+    (* Find the register associated to the variable *)
     let register = associate_register id in
     (*if(id = last_id) then Stack.push actual_block var_to_reg;*)
     generate (Rtltree.Embinop(Ops.Mmov, register, destr, destl));
@@ -105,7 +117,7 @@ let rec expr e destr destl (*last_id*) = match e with
     let initializer_register = ref Register.M.empty in
 
     let init_var label (t,id) = 
-      (* Find a register for *)
+      (* Find the register associated to the new variable *)
       let register = associate_register id in
       initializer_register:= Register.M.add register id !initializer_register;
       let instruction_assign = Rtltree.Embinop(Ops.Mmov, destr, register, label) in
@@ -115,53 +127,55 @@ let rec expr e destr destl (*last_id*) = match e with
       label_assign
     in
       
-    (* Treat the expression like an assign *)
     let label_first_init = List.fold_left init_var destl decl_list in
+    (* Convert the expression *)
     let label_expression = expr exp.expr_node destr label_first_init in
 
-    (* Add new registers in local registers and map them to the variables *)
-    let associate reg id =
+    (* Remove the mapping from the hashtable.
+    Higher expressions (before the declaration of the new variable)
+    will use variables declared in higher scopes. *)
+    let remove reg id =
       Hashtbl.remove block_var_to_reg id;
     in
-    Register.M.iter associate !initializer_register;
+    Register.M.iter remove !initializer_register;
     label_expression;
   end;
   | Ttree.Ecall (id, expr_list) -> begin
+    (* Find the function *)
     let id_param = try 
       Hashtbl.find functions id;
     with Not_found -> raise_error "No such function" in
     let call_label = Label.fresh() in
     let reg_list = ref [] in
-    let treat_params id (exp : Ttree.expr) destl =
+    (* Treat the arguments of the function *)
+    let treat_args id (exp : Ttree.expr) destl =
       let register = Register.fresh() in
       let instr_label = expr exp.expr_node register destl (*""*) in
       reg_list := register::!reg_list;
       instr_label
     in
-    let label_first_param = List.fold_right2 treat_params id_param expr_list call_label in
+    let label_first_param = List.fold_right2 treat_args id_param expr_list call_label in
     let call_instr = Rtltree.Ecall(destr, id, !reg_list, destl) in
+    (* Associate the label to the instruction *)
     graph := Label.M.add call_label call_instr !graph;
     label_first_param
   end;
   | Ttree.Eaccess_field (e,f)-> begin
-    (*print_string ("RTL Eaccess field : expression : " ^ Typing.string_of_type e.expr_typ ^ "\n");
-    print_string ("RTL Eaccess field : field : " ^ Typing.string_of_type f.field_typ ^ "\n");*)
-    (*let register_expression = Register.fresh() in*)
     let register_var = Register.fresh() in
-    let label_field = generate (Rtltree.Eload(register_var, f.field_pos * wordsize, destr, destl)) in
+    let label_field = generate (Rtltree.Eload(register_var, f.field_pos * word_size, destr, destl)) in
     let label_expression = if(e.expr_typ = Ttree.Ttypenull) then generate(Rtltree.Econst(zero_i32, register_var, label_field))
     else expr e.expr_node register_var label_field (*""*) in
     label_expression;
   end;
   | Ttree.Eassign_field (expl,f,e)-> begin
     let register_left = Register.fresh() in
-    let label_field = generate (Rtltree.Estore(destr, register_left, f.field_pos * wordsize, destl)) in
+    let label_field = generate (Rtltree.Estore(destr, register_left, f.field_pos * word_size, destl)) in
     let label_left = expr expl.expr_node register_left label_field (*""*) in
     let label_expression = expr e.expr_node destr label_left (*""*) in
     label_expression;
   end;
   | Ttree.Esizeof str -> begin
-    generate (Rtltree.Econst(Int32.of_int (str.str_size*wordsize), destr, destl))
+    generate (Rtltree.Econst(Int32.of_int (str.str_size*word_size), destr, destl))
   end;
 
 
@@ -193,7 +207,6 @@ and treat_binop e1 e2 destr destl binop (*last_id*) =
   let reg_e2 = Register.fresh() in
   let label_next = ref destl in
   let operation = match binop with
-  (* TODO : Treat the constant case *)
   | Ptree.Beq -> 
     boolean_op Ops.Msete destr destl label_next;  Ops.Msub;
   | Ptree.Bneq-> 
@@ -265,37 +278,43 @@ and condition_boolean_op binop (e1: Ttree.expr) (e2: Ttree.expr) truel falsel =
   | Ptree.Bgt -> r1 := reg_e2; r2 := reg_e1; Ops.Mjl
   | Ptree.Bge -> r1 := reg_e2; r2 := reg_e1; Ops.Mjle
   | _ -> raise_error "ha" in
-  let instruction_branch = Embbranch(op, !r2, !r1, truel, falsel) in
+  (*let instruction_branch = Embbranch(op, !r2, !r1, truel, falsel) in
   let label_branch = generate instruction_branch in
   let label_put_e2 = expr e2.expr_node reg_e2 label_branch (*""*) in
   let label_put_e1 = expr e1.expr_node reg_e1 label_put_e2 (*""*) in
-  label_put_e1
-  (*
-  let immediate_op_r1 c = match binop with (*TODO: UNCOMPLETE*)
-  | Ptree.Blt -> Ops.Mjgi c
-  | Ptree.Ble -> Ops.Mjlei c
-  | Ptree.Bgt -> Ops.Mgi c
-  | Ptree.Bge -> Ops.Mjlei c
-  | _ -> raise_error "Treat_immediate : case should not exist"
-
-  let immediate_op_r2 c = match binop with
-  | Ptree.Blt -> Ops.Mjgi c
-  | Ptree.Ble -> Ops.Mjlei c
-  | Ptree.Bgt -> Ops.Mgi c
-  | Ptree.Bge -> Ops.Mjlei c
-  | _ -> raise_error "Treat_immediate : case should not exist"
-  in
-  match e1.expr_node, e2.expr_node with
-  | (_, Ttree.Econst c) -> begin
-    let instruction_addi =  Emubranch(immediate_op_r2 c, !r1, truel, falsel) in
-    let label_addi = generate instruction_addi in
-    let label_put_e1 = expr e1.expr_node destr label_addi in
+  label_put_e1*)
+  match e1.expr_node, e2.expr_node, binop with
+  | (Ttree.Econst c1, Ttree.Econst c2, _) -> begin
+    let id, im_op = match binop with
+      | Ptree.Bgt -> 1, Ops.Mjgi c2;
+      | Ptree.Ble -> 1, Ops.Mjlei c2;
+      | Ptree.Blt -> 2, Ops.Mjgi c1;
+      | Ptree.Bge -> 2, Ops.Mjlei c1;
+      | _ -> raise_error "ha"
+    in
+    if(id = 1) then begin
+      let instruction_branch = Emubranch(im_op, reg_e1, truel, falsel) in
+      let label_branch = generate instruction_branch in
+      let label_put_e1 = expr e1.expr_node reg_e1 label_branch (*""*) in
+      label_put_e1
+    end
+    else begin
+      let instruction_branch = Emubranch(im_op, reg_e2, truel, falsel) in
+      let label_branch = generate instruction_branch in
+      let label_put_e2 = expr e2.expr_node reg_e2 label_branch (*""*) in
+      label_put_e2
+    end
+  end
+  | (_, Ttree.Econst c, Ptree.Ble) -> begin
+    let instruction_branch =  Emubranch(Ops.Mjlei c, reg_e1, truel, falsel) in
+    let label_branch = generate instruction_branch in
+    let label_put_e1 = expr e1.expr_node reg_e1 label_branch in
     label_put_e1
   end
-  | (Ttree.Econst c, _) -> begin
-    let instruction_addi =  Emubranch(immediate_op_r1 c, !r2, truel, falsel) in
-    let label_addi = generate instruction_addi in
-    let label_put_e2 = expr e2.expr_node destr label_addi in
+  | (Ttree.Econst c, _, Ptree.Blt) -> begin
+    let instruction_branch =  Emubranch(Ops.Mjgi c, reg_e2, truel, falsel) in
+    let label_branch = generate instruction_branch in
+    let label_put_e2 = expr e2.expr_node reg_e2 label_branch in
     label_put_e2
   end
   | _ -> begin
@@ -303,8 +322,8 @@ and condition_boolean_op binop (e1: Ttree.expr) (e2: Ttree.expr) truel falsel =
     let label_branch = generate instruction_branch in
     let label_put_e2 = expr e2.expr_node reg_e2 label_branch in
     let label_put_e1 = expr e1.expr_node reg_e1 label_put_e2 in
+    label_put_e1
   end
-  *)
 
 and condition e truel falsel = match e with
   |Ttree.Ebinop (binop, e1, e2) -> (match binop with
