c57467ff0e31fedc3a0efaaa8ed8a78793a462dc
amine-chaabouni <amine.chaabouni@polytechnique.edu>
2021-03-12 10:21:01 +0100

    improve test

--- tests/exec/initialize4.c
+++ tests/exec/initialize4.c
@@ -7,15 +7,16 @@ int f(int b, int r, int a, int v, int o){
 }
 
 int print_bravo(int a){
-    int b = a+1;
-    int r = a+17;
+    int b = a;
+    int r = a+16;
+    int a = 65;
     int v = a+21;
     int o = a+14;
     f(b,r,a,v,o);
 }
 
 int main() {
-  int x = 65;
+  int x = 66;
   print_bravo(x);
   putchar(10);
   return 0;
